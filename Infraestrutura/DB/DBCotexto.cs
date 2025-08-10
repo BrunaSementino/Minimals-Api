b681504d42c45aa9481e4abbae918ddcf0a1c8ea
Bruna Sementino <brunasementino@gmail.com>
2025-08-09 22:11:20 -0300

    iniciando projeto

---
+++ Infraestrutura/DB/DBCotexto.cs
@@ -0,0 +1,7 @@
+namespace   MinimalApi.Infraestrutura.DB;
+
+public class DBCotexto
+{
+    // This class is intended to manage the database context for the application.
+    // It will include methods for connecting to the database, executing queries, etc.
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World from .NET 9!");
+
+app.Run();
+
+app.MapPost("/Login", (Dominio.DTOs.LoginDTO LoginDTO) =>
+{
+    if (LoginDTO.Email == "adm@teste.com" && LoginDTO.Password == "123456")
+    {
+        return Results.Ok("Login successful");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+public class LoginDTO
+{
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+} 
